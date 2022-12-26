9d9e21b61237445c7eece3085f0c0e2fd1b21c7e
chotiwit.o <Chotiwit.O@arise.tech>
2022-12-26 15:37:08 +0700

    update reconcile_mock func

--- repositories/reconcile.go
+++ repositories/reconcile.go
@@ -11,7 +11,7 @@ type Reconcile struct {
 	TransactionRefID             string    `gorm:"colum:transaction_ref_id"`
 	Status                       string    `gorm:"colum:status"`
 	TransactionCreatedTimestamp  time.Time `gorm:"colum:transaction_created_timestamp"`
-	PaymentInfoAmount            float32       `gorm:"colum:payment_info_amount"`
+	PaymentInfoAmount            float32   `gorm:"colum:payment_info_amount"`
 	PaymentInfoWebAdditionalInfo string    `gorm:"colum:payment_info_web_additional_info"`
 	PartnerInfoName              string    `gorm:"colum:partner_info_name"`
 	PartnerInfoDeeplinkUrl       string    `gorm:"colum:partner_info_deeplink_url"`
@@ -20,10 +20,10 @@ type Reconcile struct {
 	PlanCode                     string    `gorm:"colum:plan_code"`
 	PlanName                     string    `gorm:"colum:plan_name"`
 	EffectiveDate                time.Time `gorm:"colum:effective_date"`
-	ExpireDate                   time.Time    `gorm:"colum:expire_date"`
-	IssueDate                    time.Time    `gorm:"colum:issue_date"`
+	ExpireDate                   time.Time `gorm:"colum:expire_date"`
+	IssueDate                    time.Time `gorm:"colum:issue_date"`
 	InsuranceStatus              string    `gorm:"colum:insurance_status"`
-	TotalSumInsured              float32    `gorm:"colum:total_sum_insured"`
+	TotalSumInsured              float32   `gorm:"colum:total_sum_insured"`
 	ProductOwner                 string    `gorm:"colum:product_owner"`
 	PlanType                     string    `gorm:"colum:plan_type"`
 }
@@ -32,23 +32,23 @@ type InsulerCallback struct {
 }
 type Alert struct {
 	gorm.Model
-	Messages  string `gorm:"colum:messages"`
-	Count     int `gorm:"colum:count"`
-	Status    string `gorm:"colum:status;default:Fail"`
+	Messages  string    `gorm:"colum:messages"`
+	Count     int       `gorm:"colum:count"`
+	Status    string    `gorm:"colum:status;default:Fail"`
 	NextAlert time.Time `gorm:"colum:next_alert"`
-	RefId     string `gorm:"colum:ref_id"`
-	Missing   string `gorm:"colum:missing"`
+	RefId     string    `gorm:"colum:ref_id"`
+	Missing   string    `gorm:"colum:missing"`
 }
 
 type ReconcileRepository interface {
 	GetReconcileByRefID(id string) (reconciles []Reconcile, err error)
 	SaveReconcile(reconcile Reconcile) error
-	UpdateReconcile()()
+	UpdateReconcile()
 
 	GetStatusReconcileFail()
 
-	GetAlertFail()()
+	GetAlertFail()
 
 	SaveAlert(alert Alert) error
-	UpdateAlertStatus()()
+	UpdateAlertStatus()
 }
--- repositories/reconcile_db.go
+++ repositories/reconcile_db.go
@@ -13,20 +13,16 @@ func NewReconcileRepositoryDB(db *gorm.DB) reconcileRepositoryDB {
 	db.Table("tbl_purchase_alert").AutoMigrate(&Alert{})
 	return reconcileRepositoryDB{db}
 }
-func (obj reconcileRepositoryDB)GetReconcileByRefID(id string) (reconciles []Reconcile, err error){
-	err = obj.db.Where("transaction_ref_id=?",id).Find(&reconciles).Error
-	return reconciles, err
-}
-func (obj reconcileRepositoryDB)GetReconcileByRefID(id string) (reconciles []Reconcile, err error){
-	err = obj.db.Where("transaction_ref_id=?",id).Find(&reconciles).Error
+
+func (obj reconcileRepositoryDB) GetReconcileByRefID(id string) (reconciles []Reconcile, err error) {
+	err = obj.db.Where("transaction_ref_id=?", id).Find(&reconciles).Error
 	return reconciles, err
 }
 
-func (obj reconcileRepositoryDB) SaveReconcile(reconcile Reconcile) error{
+func (obj reconcileRepositoryDB) SaveReconcile(reconcile Reconcile) error {
 	return obj.db.Create(&Reconcile{}).Error
 }
 
-func (obj reconcileRepositoryDB)SaveAlert(alert Alert) error{
+func (obj reconcileRepositoryDB) SaveAlert(alert Alert) error {
 	return obj.db.Create(&Alert{}).Error
 }
-
