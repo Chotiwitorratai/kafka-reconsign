aa1639bf5203a8d3c1941310fc6bcb48f41520bb
chotiwit.o <Chotiwit.O@arise.tech>
2022-12-27 13:50:17 +0700

    update

--- repositories/reconcile_db.go
+++ repositories/reconcile_db.go
@@ -1,7 +1,6 @@
 package repositories
 
 import (
-
 	"gorm.io/gorm"
 )
 
@@ -15,39 +14,38 @@ func NewReconcileRepositoryDB(db *gorm.DB) reconcileRepositoryDB {
 	return reconcileRepositoryDB{db}
 }
 
-
-
-func (obj reconcileRepositoryDB)CheckNullReconcile(id string) (boo bool,err error){
+func (obj reconcileRepositoryDB) CheckNullReconcile(id string) (boo bool, err error) {
 	reconciles := []Reconcile{}
-	err = obj.db.Table("tbl_purchase_reconcile").Where("transaction_ref_id=?",id).Find(&reconciles).Error
+	err = obj.db.Table("tbl_purchase_reconcile").Where("transaction_ref_id=?", id).Find(&reconciles).Error
 	if len(reconciles) == 1 {
-		return true , err
-	}else {
-		return false , err
+		return true, err
+	} else {
+		return false, err
 	}
 }
-func (obj reconcileRepositoryDB) SaveReconcile(reconcile Reconcile) error{
+
+func (obj reconcileRepositoryDB) SaveReconcile(reconcile Reconcile) error {
 	return obj.db.Table("tbl_purchase_reconcile").Create(&reconcile).Error
 }
-func (obj reconcileRepositoryDB)UpdateReconcile(reconcile Reconcile) error{
-	return obj.db.Table("tbl_purchase_reconcile").Where("transaction_ref_id=?",reconcile.TransactionRefID).Updates(&reconcile).Error
+
+func (obj reconcileRepositoryDB) UpdateReconcile(reconcile Reconcile) error {
+	return obj.db.Table("tbl_purchase_reconcile").Where("transaction_ref_id=?", reconcile.TransactionRefID).Updates(&reconcile).Error
 }
-func (obj reconcileRepositoryDB)GetReconcileFail()(reconcile []Reconcile, err error){
+
+func (obj reconcileRepositoryDB) GetReconcileFail() (reconcile []Reconcile, err error) {
 	err = obj.db.Table("tbl_purchase_reconcile").Where("status = '' || insurance_status = '' ").Find(&reconcile).Error
-	return reconcile , err
+	return reconcile, err
 }
 
-
-func (obj reconcileRepositoryDB)SaveAlert(alert []Alert) error{
+func (obj reconcileRepositoryDB) SaveAlert(alert []Alert) error {
 	return obj.db.Table("tbl_purchase_alert").Create(&alert).Error
 }
 
-func (obj reconcileRepositoryDB)UpdateAlert(alert Alert) error{
-	return obj.db.Table("tbl_purchase_alert").Where("ref_id=?",alert.RefId).Updates(&alert).Error
+func (obj reconcileRepositoryDB) UpdateAlert(alert Alert) error {
+	return obj.db.Table("tbl_purchase_alert").Where("ref_id=?", alert.RefId).Updates(&alert).Error
 }
-func (obj reconcileRepositoryDB)GetAlertFail()(alert []Alert, err error){
+
+func (obj reconcileRepositoryDB) GetAlertFail() (alert []Alert, err error) {
 	err = obj.db.Table("tbl_purchase_alert").Where("status = 'Fail'").Find(&alert).Error
-	return alert , err
-
+	return alert, err
 }
-
